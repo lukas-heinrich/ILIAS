d3f192487d1721ce0089ce64ffbc94c1ab8bd7ae
Lukas Eichenauer <leichenauer@databay.de>
2026-04-07 16:56:24 +0200

    feat: introduce question pool import stages

---
+++ components/ILIAS/TestQuestionPool/src/ExportImport/Import/PersistStage.php
@@ -0,0 +1,75 @@
+<?php
+
+/**
+ * This file is part of ILIAS, a powerful learning management system
+ * published by ILIAS open source e-Learning e.V.
+ *
+ * ILIAS is licensed with the GPL-3.0,
+ * see https://www.gnu.org/licenses/gpl-3.0.en.html
+ * You should have received a copy of said license along with the
+ * source code, too.
+ *
+ * If this is not the case or you just want to try ILIAS, you'll find
+ * us at:
+ * https://www.ilias.de
+ * https://github.com/ILIAS-eLearning
+ *
+ *********************************************************************/
+
+declare(strict_types=1);
+
+namespace ILIAS\TestQuestionPool\ExportImport\Import;
+
+use ILIAS\Language\Language;
+use ILIAS\Questions\ExportImport\Foundation\Contracts\ImportStage;
+use ILIAS\Questions\ExportImport\Foundation\Importing\ImportContext;
+use ILIAS\Questions\ExportImport\Foundation\Importing\StageResult;
+use ILIAS\TestQuestionPool\RequestDataCollector;
+use ilImport;
+use Psr\Http\Message\ServerRequestInterface;
+
+/**
+ * Final stage of the question pool import process. Imports the question pool object and all its questions and other
+ * dependencies using `ilImport`. It will delegate the import to the `ilTestQuestionPoolImporter` class.
+ */
+class PersistStage implements ImportStage
+{
+    public function __construct(
+        private readonly Language $lng,
+        private readonly RequestDataCollector $request_data_collector
+    ) {
+    }
+
+    public function getIdentifier(): string
+    {
+        return 'persist';
+    }
+
+    public function getLabel(): string
+    {
+        return $this->lng->txt('qpl_import_step_persist');
+    }
+
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function process(ImportContext $context, ServerRequestInterface $request): StageResult
+    {
+        $importer = new ilImport($this->request_data_collector->getRefId());
+        $importer->importObject(
+            null,
+            $context->get('file_to_import'),
+            basename($context->get('file_to_import')),
+            'qpl',
+            'components/ILIAS/TestQuestionPool',
+            true,
+        );
+
+        $mappings = $importer->getMapping()->getMappingsOfEntity('components/ILIAS/TestQuestionPool', 'qpl');
+        $new_pool_id = array_pop($mappings);
+
+        return StageResult::complete($context->with('pool_obj_id', $new_pool_id));
+    }
+}
